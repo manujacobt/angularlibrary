7d53ad7a8d4cda6c918bb721b72412faf23b28cf
manujacobt <manujacobt@gmail.com>
2021-12-02 15:54:54 +0530

    3rd

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ app.use('/uploads', express.static('uploads'))
 
 app.set('views', './src/views');
 app.use(methodoverride('_method'));
-app.use('/api/books', booksRouter);
+app.use('/books', booksRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/signup', signupRouter);
 app.use('/api/addbook', adminRouter);
 app.use('/api/addauthor', admin1Router);
-app.use('/api/authors', authorRouter);
+app.use('/authors', authorRouter);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist//Frontend/index.html'));});
